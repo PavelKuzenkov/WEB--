46545f3d2f4e522c76cb8e0a033dc8fa897e3e43
pavelkuzenkov <pavel.kuzenkov@dz.ru>
2019-11-25 19:21:42 +0400

    Замена заголовков новостей на числа (для нейронки нужны числа). По моему мнению, код наикорявейший. Нужен рефакторинг.

--- src/main/java/kuzenkov/investing_news_downloader/MyCsv.java
+++ src/main/java/kuzenkov/investing_news_downloader/MyCsv.java
@@ -24,7 +24,7 @@ public class MyCsv {
     private static final String NONFARM_PAYROLLS = "Nonfarm Payrolls.*";
     private static final String PERSONAL_INCOME = "Personal Income.*";
     private static final String PERSONAL_SPENDING = "Personal Spending.*";
-    private static final String PRODUCTIVITY = "Productivity.*";
+    private static final String NONFARM_PRODUCTIVITY = "Nonfarm Productivity.*(9)";
     private static final String JOBLESS_CLAIMS = "Initial Jobless Claims.*";
     private static final String CPI = ".*CPI.*";
     private static final String PPI = ".*PPI.*";
@@ -55,7 +55,7 @@ public class MyCsv {
                             NONFARM_PAYROLLS,
                             PERSONAL_INCOME,
                             PERSONAL_SPENDING,
-                            PRODUCTIVITY,
+                            NONFARM_PRODUCTIVITY,
                             JOBLESS_CLAIMS,
                             CPI,
                             PPI,
@@ -83,7 +83,8 @@ public class MyCsv {
           // first create file object for file placed at location
           // specified by filepath
           File file = new File("usa-2010-01-01-2019-01-07.csv");
-          File pfile = new File("usa-2010-01-01-2019-01-07-result.csv");
+          File resultFile = new File("usa-2010-01-01-2019-01-07-result.csv");
+          File resultFileWithNumbers = new File("usa-2010-01-01-2019-01-07-resultWithNumbers.csv");
           List<String[]> result = new ArrayList<>();
           try (FileReader input = new FileReader(file);
               CSVReader reader = new CSVReader(input, ';')) {
@@ -93,23 +94,71 @@ public class MyCsv {
               e.printStackTrace();
           }
 
-          try (FileWriter outputfile = new FileWriter(pfile);
+          try (FileWriter outputfile = new FileWriter(resultFile);
                CSVWriter writer = new CSVWriter(outputfile, ';')) {
 
               // adding header to csv
               String[] header = result.get(0);
               writer.writeNext(header);
+              List<String[]> removeList = new ArrayList<>();
 
               for (String[] string : result) {
                   if (isNeededNews(string[3])) {
                       writer.writeNext(string);
+                  } else {
+                      removeList.add(string);
                   }
+              }
+              removeList.remove(0);
+              result.removeAll(removeList);
+          }
+          catch (IOException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+          }
+
+          try (FileWriter outputfile = new FileWriter(resultFileWithNumbers);
+               CSVWriter writer = new CSVWriter(outputfile, ';')) {
+
+//              // adding header to csv
+//              String[] header = result.get(0);
+//              writer.writeNext(header);
+
+              for (String[] string : result) {
+                  string[3] = string[3].replaceAll(GDP, "1");
+                  string[3] = string[3].replaceAll(CURRENT_ACCOUNT, "2");
+                  string[3] = string[3].replaceAll(TRADE_BALANCE, "3");
+                  string[3] = string[3].replaceAll(FEDERAL_BUDGET, "4");
+                  string[3] = string[3].replaceAll(UNEMPLOYTMENT_RATE, "5");
+                  string[3] = string[3].replaceAll(NONFARM_PAYROLLS, "6");
+                  string[3] = string[3].replaceAll(PERSONAL_INCOME, "7");
+                  string[3] = string[3].replaceAll(PERSONAL_SPENDING, "8");
+                  string[3] = string[3].replaceAll(NONFARM_PRODUCTIVITY, "9");
+                  string[3] = string[3].replaceAll(JOBLESS_CLAIMS, "10");
+                  string[3] = string[3].replaceAll(CPI, "11");
+                  string[3] = string[3].replaceAll(PPI, "12");
+                  string[3] = string[3].replaceAll(BUILDING_PERMITS, "13");
+                  string[3] = string[3].replaceAll(HOUSING_STARTS, "14");
+                  string[3] = string[3].replaceAll(NEW_HOME_SALES, "15");
+                  string[3] = string[3].replaceAll(CONSTRUCTION_SPENDING, "16");
+                  string[3] = string[3].replaceAll(EXISTING_HOME_SALES, "17");
+                  string[3] = string[3].replaceAll(CORE_RETAIL_SALES, "18");
+                  string[3] = string[3].replaceAll(RETAIL_SALES, "19");
+                  string[3] = string[3].replaceAll(DURABLE_GOODS_ORDERS, "20");
+                  string[3] = string[3].replaceAll(INDUSTRIAl_PRODUCTION, "21");
+                  string[3] = string[3].replaceAll(CAPACITY_UTILIZATION, "22");
+                  string[3] = string[3].replaceAll(FACTORY_ORDERS, "23");
+                  string[3] = string[3].replaceAll(PMI, "24");
+                  string[3] = string[3].replaceAll(CONSUMER_CONFIDENCE, "25");
+                  string[3] = string[3].replaceAll(MICHIGAN_CONSUMER_SENTIMENT, "26");
+                  writer.writeNext(string);
               }
           }
           catch (IOException e) {
               // TODO Auto-generated catch block
               e.printStackTrace();
           }
+
       }
 
     private static boolean isNeededNews(String number) {
